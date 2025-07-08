07f1d049c93283beab4921dba48079bf78f47140
Vaios0x <vaiogioss@gmail.com>
2025-07-08 15:03:34 -0600

    Actualiza configuración de Vite para despliegue en Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3007
+  },
+  build: {
+    outDir: 'dist'
   }
 })
